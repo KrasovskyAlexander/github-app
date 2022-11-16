a2f910deb0d30589b68bacb2404850819e3b04fb
Alexander Krasovsky <Alexander_Krasovsky@epam.com>
2022-11-16 12:42:56 +0300

    connect redux with react

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 
 import './App.scss';
+import { useTypedSelector } from './hooks/useTypedSelector';
+import { fetchUser } from './store/actions/usersActions';
 
 function App() {
+  const users= useTypedSelector(state => state.users);
+  const dispatch = useDispatch();
+
+  console.log(users);
+
   return (
     <div className="App">
-
+      <button onClick={() => dispatch(fetchUser())}>fetch</button>
+      {!users.loading ? users.users.map(user => (
+        <div key={user.id}>{user.name}</div>
+      )) : <div>ЗАГРУЗКА</div>
+      }
     </div>
   );
 }
